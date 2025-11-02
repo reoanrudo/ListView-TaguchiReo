8abde5f0d12169ac184e65bec863222747c38984
reo <sodapon03@gmail.com>
2025-11-02 10:53:42 +0900

    Initial Commit

---
+++ ListView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListView
+//
+//  Created by 田口怜央 on 2025/11/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by 田口怜央 on 2025/11/02.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
